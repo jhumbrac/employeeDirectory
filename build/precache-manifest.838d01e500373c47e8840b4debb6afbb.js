83bdfbb8655fc97bd595e15ced2b430b8daaf2ef
jhumbrac <john.humbracht@generalassemb.ly>
2020-01-21 10:03:05 -0600

    tuesday morning

---
+++ build/precache-manifest.838d01e500373c47e8840b4debb6afbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb1ce7764a3d121b8f04e41fb703e51",
+    "url": "/employeeDirectory/index.html"
+  },
+  {
+    "revision": "2672a804cd344f8f1c4a",
+    "url": "/employeeDirectory/static/css/main.a825e55e.chunk.css"
+  },
+  {
+    "revision": "7a1b5cdb84235519efca",
+    "url": "/employeeDirectory/static/js/2.b412a55a.chunk.js"
+  },
+  {
+    "revision": "abf9dce7bfc91acc1f76f4a3096f8868",
+    "url": "/employeeDirectory/static/js/2.b412a55a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2672a804cd344f8f1c4a",
+    "url": "/employeeDirectory/static/js/main.7de308fa.chunk.js"
+  },
+  {
+    "revision": "3b63922034ee69a58c36",
+    "url": "/employeeDirectory/static/js/runtime-main.58ea8a3c.js"
+  },
+  {
+    "revision": "d52744e02753ced11382a258d455d072",
+    "url": "/employeeDirectory/static/media/SpongeBob.d52744e0.ttf"
+  }
+]);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,10 +3,15 @@ import "./style.css";
 
 export default function Home() {
     return(
-        <div className="coming-soon">
-            <h1>This is my Home page</h1>
-            <p>This page is under construction. Please come back later to see the finished product.</p>
-            <h2>In the mean time please click on the "Evil Plans" link to see my homework.</h2>
+        <div className="page">
+            <div className="coming-soon">
+                <h1>This is my Home page</h1>
+                <p>This page is under construction. Please come back later to see the finished product.</p>
+                <h2>In the mean time please click on the "Evil Plans" link to see my homework.</h2>
+            </div>
+            <section id="intro">
+                <img src="images/chum-23hours.jpg" alt="Now open 23 hours" />
+            </section>
         </div>
     )
 }
